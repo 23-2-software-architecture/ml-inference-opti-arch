6f49824311507114bc1398ad2fde848a7bf7d119
mhsong <mhsong@kookmin.ac.kr>
2023-11-07 15:22:55 +0900

    Add kubernetes iac code (AWS)

---
+++ backend/IaC/k8s/variable.tf
@@ -0,0 +1,10 @@
+variable "vpc" {}
+variable "private_subnet_ids" {}
+variable "cluster_prefix" {}
+variable "master_node_number" {}
+variable "worker_node_number" {}
+variable "instance_type" {}
+variable "ubuntu_ami" {}
+variable "key_name" {}
+variable "ec2_instance_profile" {}
+# variable "monitoring-efs-id" {}
