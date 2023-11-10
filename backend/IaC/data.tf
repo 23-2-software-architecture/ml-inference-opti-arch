670519071a1c4e4740794a8b6f393e83a65fa240
mhsong <mhsong@kookmin.ac.kr>
2023-11-10 16:12:08 +0900

    Update AMI INfo

--- backend/IaC/data.tf
+++ backend/IaC/data.tf
@@ -12,7 +12,7 @@ data "aws_ami" "ubuntu_ami" {
   most_recent = true
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-arm64-server*"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server*"]
   }
   filter {
     name   = "virtualization-type"
