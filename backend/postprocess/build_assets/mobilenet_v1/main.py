70694fb68a174acb08ee1121213df372e8460f5d
seungwoo1124 <seungwoo1124@naver.com>
2023-11-10 17:30:39 +0900

    modify postprocess_2

--- backend/postprocess/build_assets/mobilenet_v1/main.py
+++ backend/postprocess/build_assets/mobilenet_v1/main.py
@@ -3,17 +3,18 @@ import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import numpy as np
 from fastapi import FastAPI
+import pickle
 app = FastAPI()
+with open('imagenet_labels.pkl', 'rb') as f:
+    labels = pickle.load(f)
 
 @app.post('/')
 async def predict(json_body: dict):
-    with open('imagenet_labels.txt', 'r') as f:
-        labels = f.read().splitlines()
-
     predictions = np.array(json_body['body'])
     predicted_class_index = np.argmax(predictions)
 
-    predicted_label = labels[predicted_class_index + 1]
+    predicted_label = labels[predicted_class_index + 1].split()
+    predicted_label = predicted_label[-1]
     predicted_probability = predictions[0][predicted_class_index]
 
     response = {
