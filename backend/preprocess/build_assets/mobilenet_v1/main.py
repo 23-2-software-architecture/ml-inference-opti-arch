1ce5802aa159c47c41c685dd16f3721e106e4a1c
mhsong <mhsong@kookmin.ac.kr>
2023-11-10 15:36:10 +0900

    Add preprocess module

---
+++ backend/preprocess/build_assets/mobilenet_v1/main.py
@@ -0,0 +1,21 @@
+import numpy as np
+from fastapi import FastAPI, UploadFile, File
+from typing import Optional
+from PIL import Image
+import json
+
+app = FastAPI()
+
+@app.get('/preprocess')
+async def preprocess_get():
+    return "Welcome to preprocess module"
+
+@app.post('/preprocess')
+async def predict(file: Optional[UploadFile] = File(None)):
+    image_file = await file.read()
+    img = Image.open(image_file)
+    img = img.resize((224, 224))
+    img_array = np.array(img)
+    img_array = img_array.astype('float32') / 255.0
+    img_array = np.expand_dims(img_array, axis=0)
+    return json.dumps({"inputs": {"input_1": img_array.tolist()}})
