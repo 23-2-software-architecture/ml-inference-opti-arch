d65db8da1b9291a514fcab7c7b16ce0a79b6c279
seungwoo1124 <seungwoo1124@naver.com>
2023-11-10 16:30:36 +0900

    modify postprocess

--- backend/postprocess/build_assets/mobilenet_v1/main.py
+++ backend/postprocess/build_assets/mobilenet_v1/main.py
@@ -4,9 +4,21 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import numpy as np
 from fastapi import FastAPI
 app = FastAPI()
-from tensorflow.keras.models import load_model
-model = load_model('./mobilenet_v1')
 
 @app.post('/')
 async def predict(json_body: dict):
     ### add code here
+    with open('imagenet_labels.txt', 'r') as f:
+        labels = f.read().splitlines()
+
+    predictions = np.array(json_body['body'])
+    predicted_class_index = np.argmax(predictions)
+
+    predicted_label = labels[predicted_class_index]
+    predicted_score = predictions[0][predicted_class_index]
+
+    response = {
+        'result': predicted_label
+    }
+
+    return response
